af567e01fd99eafd8a0398bee8c76008c3730fad
David <David.dennis@welldata.co.uk>
2024-08-19 14:57:31 +0100

    Initial Commit

---
+++ schema-model/Tables/dbo.DDTestWidgetType.sql
@@ -0,0 +1,22 @@
+USE [WidgetDev]
+GO
+
+/****** Object:  Table [dbo].[WidgetPrices]    Script Date: 19/08/2024 14:52:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DDTestWidgetType](
+	[TypeID] [int] IDENTITY(1,1) NOT NULL,
+	[WidgetID] [int] NULL,
+	[Price] [decimal](18, 0) NULL,
+ CONSTRAINT [PK_WidgetTypes] PRIMARY KEY NONCLUSTERED 
+(
+	[TypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
