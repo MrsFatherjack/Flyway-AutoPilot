d747f9ab7a09434c9ad3ea53b21eee8000c3a273
Annette Allen <annette.allen@welldata.co.uk>
2024-08-20 12:02:09 +0100

    Test commit

---
+++ migrations/V008__Annette_Allen.sql
@@ -0,0 +1,21 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[AnnetteTestNew]'
+GO
+CREATE TABLE [dbo].[AnnetteTestNew]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[Newcol] [varchar] (100) NULL
+)
+GO
+PRINT N'Creating [dbo].[AnnetteTest]'
+GO
+CREATE TABLE [dbo].[AnnetteTest]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[Newcol] [varchar] (100) NULL
+)
+GO
+
---
+++ schema-model/Tables/dbo.AnnetteTestNew.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[AnnetteTestNew]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[Newcol] [varchar] (100) NULL
+)
+GO
