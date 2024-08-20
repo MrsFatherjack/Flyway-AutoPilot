7e6dabfd8c3c41e684e15270e1ab8a669bc2ea9c
Annette Allen <annette.allen@welldata.co.uk>
2024-08-20 09:48:05 +0100

    Test commit

---
+++ migrations/V007__Annette_Allen.sql
@@ -0,0 +1,18 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[DDTestWidgetType]'
+GO
+CREATE TABLE [dbo].[DDTestWidgetType]
+(
+[TypeID] [int] NOT NULL IDENTITY(1, 1),
+[WidgetID] [int] NULL,
+[Price] [decimal] (18, 0) NULL
+)
+GO
+PRINT N'Creating primary key [PK_WidgetTypes] on [dbo].[DDTestWidgetType]'
+GO
+ALTER TABLE [dbo].[DDTestWidgetType] ADD CONSTRAINT [PK_WidgetTypes] PRIMARY KEY NONCLUSTERED ([TypeID])
+GO
+
